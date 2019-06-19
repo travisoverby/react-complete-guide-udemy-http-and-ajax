372338aa88ed3e3ab5ccdaa7decad1dbad4f5548
Travis Overby <tek62441@adobe.com>
2019-06-18 17:23:48 -0700

    Simulate GET, POST, DELETE requests and use Axios interceptors

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,61 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  state = {
+    posts: [],
+    selectedPostId: null,
+    error: false
+  };
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then(response => {
+          const posts = response.data.slice(0, 4);
+          const updatedPosts = posts.map(post => {
+            return {
+              ...post,
+              author: 'Max'
+            };
+          });
+          this.setState({ posts: updatedPosts });
+          // console.log(response);
+        })
+        .catch(error => {
+          console.log(error);
+          this.setState({ error: true });
+        });
+  }
+
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id });
+  }
+
+  render () {
+    let posts = <p style={{ textAlign: 'center' }}>Something went wrong!</p>;
+    if (!this.state.error) {
+      posts = this.state.posts.map(post => {
+        return <Post 
+                key={post.id} 
+                title={post.title} 
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />;
+      });
     }
+
+    return (
+      <div>
+        <section className="Posts">
+          {posts}
+        </section>
+        <section>
+          <FullPost id={this.state.selectedPostId}/>
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
